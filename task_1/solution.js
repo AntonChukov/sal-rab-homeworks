20599569195ecfdd2e76cfdc972c4c2557f707b9
Chukov Anton Maksimovich <Chukov.A.M@sblogistica.ru>
2023-03-31 17:56:16 +0300

    Add task1 soution

--- task_1/solution.js
+++ task_1/solution.js
@@ -6,13 +6,16 @@ function addInCartMessage(title, price) {
 
     // создайте переменную message
     // присвойте переменной message значение переменной productName
+    let message = productName;
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message текст 'за'
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message значение переменной productPrice
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message текст 'теперь в корзине!'
+    message = message + ' за ' + productPrice + ' теперь в корзине!'
     // выведите переменную message в консоль
+    console.log(message);
 
     // Конец решения задания №1.1.
 
@@ -26,7 +29,9 @@ function addInCartCountChange(value) {
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
+    let newValue = oldValue;
     // увеличьте значение newValue на 1
+    newValue = newValue + 1;
 
     // Конец решения задания №1.2.
 
@@ -40,8 +45,12 @@ function addInCartSumChange(sum, delta) {
     // Задание №1.3. Увеличение суммы в корзине
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
+    let newSum = oldSum + difference;
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
+    let newSumText = newSum + ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
+    newSumText = `${newSum} ₽`;
+
 
     // Конец решения задания №1.3.
 
